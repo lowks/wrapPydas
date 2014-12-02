9c46da20df5bbc2c38eae8d71f7b71e6dfeb88b3
Brian Chapman <brian.chapman@utah.edu>
2014-12-02 00:12:23 -0700

    modified setup.py to be more compliant with standard

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="wrapPydas",
         version="0.1.2",
         author="Brian Chapman",
         author_email="brian.chapman@utah.edu",
         package_dir={'wrapPydas':'src'},
-        install_requires="pydas",
+        url="https://github.com/chapmanbe/wrapPydas",
+        install_requires=["pydas"],
         packages=['wrapPydas'],
     )
